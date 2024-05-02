10e24b5a44ad60df3d4ea6e6cb0c81a6b9c49409
skpntsv <n.skopintsev@g.nsu.ru>
2024-05-02 13:45:48 +0700

    fix and add if not exists

--- db/migrations/schema.sql
+++ db/migrations/schema.sql
@@ -1,27 +1,27 @@
-CREATE TABLE Tourist_type (
+CREATE TABLE IF NOT EXISTS Tourist_type (
 	id    SERIAL 		PRIMARY KEY,
 	name  VARCHAR(100) 	NOT NULL UNIQUE
 );
 
-CREATE TABLE Specialization (
+CREATE TABLE IF NOT EXISTS Specialization (
 	id    SERIAL 		PRIMARY KEY,
 	name  VARCHAR(100) 	NOT NULL UNIQUE
 );
 
-CREATE TABLE Hike_type (
+CREATE TABLE IF NOT EXISTS Hike_type (
 	id    SERIAL 		PRIMARY KEY,
 	name  VARCHAR(100) 	NOT NULL UNIQUE
 );
 
-CREATE TABLE Contacts (
-	id 				SERIAL
+CREATE TABLE IF NOT EXISTS Contacts (
+	id 				SERIAL 			PRIMARY KEY,
 	email 			VARCHAR(256) 	NOT NULL CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
 	main_phone 		VARCHAR(15) 	NOT NULL CHECK (main_phone ~* '^(\+7|8)-\d{3}-\d{3}-\d{2}-\d{2}$'),
 	reserve_phone	VARCHAR(15) 	CHECK (reserve_phone ~* '^(\+7|8)-\d{3}-\d{3}-\d{2}-\d{2}$'),
 	emergency_phone VARCHAR(15) 	CHECK (emergency_phone ~* '^(\+7|8)-\d{3}-\d{3}-\d{2}-\d{2}$')
 );
 
-CREATE TABLE Tourist (
+CREATE TABLE IF NOT EXISTS Tourist (
 	id        	SERIAL 			PRIMARY KEY,
 	full_name 	VARCHAR(255) 	NOT NULL,
 	gender    	VARCHAR(7) 		NOT NULL CHECK (gender IN ('male', 'female')),
@@ -31,7 +31,7 @@ CREATE TABLE Tourist (
 	contact_id	INTEGER			NOT NULL REFERENCES Contacts(id)
 );
 
-CREATE TABLE Trainer (
+CREATE TABLE IF NOT EXISTS Trainer (
 	id                	INTEGER 	PRIMARY KEY  REFERENCES Tourist(id),
 	salary            	MONEY		NOT NULL,
 	hire_date         	DATE		NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE Trainer (
 	section_id        	INTEGER 	NOT NULL REFERENCES Section(id)
 );
 
-CREATE TABLE SuperVisor (
+CREATE TABLE IF NOT EXISTS SuperVisor (
 	id          SERIAL 			PRIMARY KEY,
 	full_name   VARCHAR(255) 	NOT NULL,
 	salary      MONEY			NOT NULL,
@@ -48,14 +48,14 @@ CREATE TABLE SuperVisor (
 	contact_id	INTEGER			NOT NULL REFERENCES Contacts(id)
 );
 
-CREATE TABLE Section (
+CREATE TABLE IF NOT EXISTS Section (
 	id            SERIAL 		PRIMARY KEY,
 	name          VARCHAR(100) 	NOT NULL UNIQUE,
 	description	  TEXT,
 	supervisor_id INTEGER 		NOT NULL UNIQUE REFERENCES SuperVisor(id)
 );
 
-CREATE TABLE Group (
+CREATE TABLE IF NOT EXISTS Group (
 	id          SERIAL 			PRIMARY KEY,
 	name        VARCHAR(100) 	NOT NULL UNIQUE,
 	description TEXT,
@@ -63,13 +63,13 @@ CREATE TABLE Group (
 	trainer_id  INTEGER 		REFERENCES Trainer(id)
 );
 
-CREATE TABLE Tourist_Groups (
+CREATE TABLE IF NOT EXISTS Tourist_Groups (
 	tourist_id  INTEGER 	REFERENCES Tourist(id),
 	group_id    INTEGER 	REFERENCES Group(id),
 	PRIMARY KEY (tourist_id, group_id)
 );
 
-CREATE TABLE Training (
+CREATE TABLE IF NOT EXISTS Training (
 	id          SERIAL 			PRIMARY KEY,
 	plane_date  TIMESTAMP		NOT NULL,
 	real_date   TIMESTAMP,
@@ -80,13 +80,13 @@ CREATE TABLE Training (
 	section_id  INTEGER 		NOT NULL REFERENCES Section(id)
 );
 
-CREATE TABLE Attendance (
+CREATE TABLE IF NOT EXISTS Attendance (
 	tourist_id  INTEGER 	REFERENCES Tourist(id),
 	training_id INTEGER 	REFERENCES Training(id),
 	PRIMARY KEY (tourist_id, training_id)
 );
 
-CREATE TABLE Route (
+CREATE TABLE IF NOT EXISTS Route (
 	id                  SERIAL 			PRIMARY KEY,
 	name                VARCHAR(100) 	NOT NULL UNIQUE,
 	length_meters       INTEGER,
@@ -95,7 +95,7 @@ CREATE TABLE Route (
 	description         TEXT
 );
 
-CREATE TABLE Hike (
+CREATE TABLE IF NOT EXISTS Hike (
 	id              SERIAL 			PRIMARY KEY,
 	plan_start_date TIMESTAMPTZ 	NOT NULL,
 	real_start_date TIMESTAMPTZ,
@@ -108,14 +108,14 @@ CREATE TABLE Hike (
         CHECK ((real_end_date IS NULL) OR (real_end_date > real_start_date))
 );
 
-CREATE TABLE Diary (
+CREATE TABLE IF NOT EXISTS Diary (
 	id      SERIAL 			PRIMARY KEY,
 	date    TIMESTAMPTZ		NOT NULL,
 	content TEXT			NOT NULL,
 	hike_id INTEGER 		NOT NULL, REFERENCES Hike(id)
 );
 
-CREATE TABLE Checkpoint (
+CREATE TABLE IF NOT EXISTS Checkpoint (
 	id          SERIAL 			PRIMARY KEY,
 	description TEXT,
 	type        VARCHAR(7) 		CHECK (type IN ('passing', 'rest')),
@@ -123,7 +123,7 @@ CREATE TABLE Checkpoint (
 	longitude   NUMERIC(4, 7) 	CHECK (longitude >= -180 AND longitude <= 180)
 );
 
-CREATE TABLE Route_Checkpoints (
+CREATE TABLE IF NOT EXISTS Route_Checkpoints (
 	id            SERIAL 	UNIQUE,
 	point_number  SMALLINT	CHECK (point_number >= 1),
 	route_id      INTEGER 	REFERENCES Route(id),
@@ -131,7 +131,7 @@ CREATE TABLE Route_Checkpoints (
 	PRIMARY KEY (point_number, route_id, checkpoint_id)
 );
 
-CREATE TABLE Sсhedule_hike_checkpoints (
+CREATE TABLE IF NOT EXISTS Sсhedule_hike_checkpoints (
 	plan_date             TIMESTAMPTZ 		NOT NULL,
 	real_date             TIMESTAMPTZ,
 	hike_id               INTEGER 			REFERENCES Hike(id),
@@ -139,13 +139,13 @@ CREATE TABLE Sсhedule_hike_checkpoints (
 	PRIMARY KEY (hike_id, route_checkpoints_id)
 );
 
-CREATE TABLE Hike_Tourists (
+CREATE TABLE IF NOT EXISTS Hike_Tourists (
 	tourist_id  INTEGER 	REFERENCES Tourist(id),
 	hike_id     INTEGER 	REFERENCES Hike(id),
 	PRIMARY KEY (tourist_id, hike_id)
 );
 
-CREATE TABLE Competition (
+CREATE TABLE IF NOT EXISTS Competition (
 	id          SERIAL 			PRIMARY KEY,
 	date        TIMESTAMPTZ		NOT NULL,
 	name        VARCHAR(255) 	NOT NULL,
@@ -153,7 +153,7 @@ CREATE TABLE Competition (
 	description TEXT
 );
 
-CREATE TABLE Tourist_Competitions (
+CREATE TABLE IF NOT EXISTS Tourist_Competitions (
 	tourist_id      INTEGER 	REFERENCES Tourist(id),
 	competition_id  INTEGER 	REFERENCES Competition(id),
 	PRIMARY KEY (tourist_id, competition_id)
