ae9269991cbc57a94522995bbda2ea94b381974c
skpntsv <n.skopintsev@g.nsu.ru>
2024-05-02 13:32:48 +0700

    update

--- db/migrations/schema.sql
+++ db/migrations/schema.sql
@@ -100,10 +100,12 @@ CREATE TABLE Hike (
 	plan_start_date TIMESTAMPTZ 	NOT NULL,
 	real_start_date TIMESTAMPTZ,
 	real_end_date   TIMESTAMPTZ,
-	is_planned      BOOLEAN			DEFAULT: 0
+	is_planned      BOOLEAN			NOT NULL DEFAULT false,
 	hike_type_id    INTEGER 		NOT NULL, REFERENCES Hike_type(id),
 	instructor_id   INTEGER 		NOT NULL, REFERENCES Tourist(id),
 	route_id        INTEGER 		NOT NULL, REFERENCES Route(id)
+    CONSTRAINT check_end_date_after_start_date 
+        CHECK ((real_end_date IS NULL) OR (real_end_date > real_start_date))
 );
 
 CREATE TABLE Diary (
@@ -156,3 +158,42 @@ CREATE TABLE Tourist_Competitions (
 	competition_id  INTEGER 	REFERENCES Competition(id),
 	PRIMARY KEY (tourist_id, competition_id)
 );
+
+
+CREATE OR REPLACE FUNCTION update_tourist_level()
+RETURNS TRIGGER AS
+$$
+DECLARE
+    hike_difficulty SMALLINT;
+BEGIN
+    -- Получаем уровень сложности похода
+    SELECT difficulty_category INTO hike_difficulty
+    FROM Hike
+    WHERE id = NEW.hike_id;
+
+    -- Проверяем, есть ли дата окончания похода и is_planned равно 1
+    IF NEW.real_end_date IS NOT NULL AND NEW.is_planned THEN
+        -- Проверяем уровень каждого туриста
+        FOR tourist_rec IN
+            SELECT t.id, t.category
+            FROM Tourist t
+            JOIN Hike_Tourists ht ON t.id = ht.tourist_id
+            WHERE ht.hike_id = NEW.hike_id
+        LOOP
+            IF tourist_rec.category < hike_difficulty THEN
+                -- Повышаем уровень туриста до уровня похода
+                UPDATE Tourist
+                SET category = hike_difficulty
+                WHERE id = tourist_rec.id;
+            END IF;
+        END LOOP;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_tourist_level_trigger
+AFTER INSERT OR UPDATE ON Hike
+FOR EACH ROW
+EXECUTE FUNCTION update_tourist_level();
