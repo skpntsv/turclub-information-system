27458dbf262c4d888bf3a3902db6f860c5663a31
skpntsv <n1skopintsev@gmail.com>
2024-05-08 20:04:26 +0700

    make home page

---
+++ backend/src/main/java/ru/nsu/skopintsev/turclub/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package ru.nsu.skopintsev.turclub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
